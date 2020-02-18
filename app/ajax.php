6f36ae5257939694dddbfbd20d94bc7ea6a67a85
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-18 17:15:51 +0300

    added js and php scripts

---
+++ app/ajax.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$city = htmlspecialchars($_POST['city']);
+$phone = htmlspecialchars($_POST['phone']);
+
+$jsonout = '{"status": "error", "message": "Упс"}';
+
+if (!empty($phone)) {
+
+	// $to = 'info@bon-l.ru, info@lymmed.ru';
+	$to = 'khripunovpp@gmail.com';
+	$subject = 'BON-L';
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; utf-8' . "\r\n";
+	$headers .= 'From: Bon-l <info@bon-l.ru>' . "\r\n";
+	$message = "<table>";
+	$message .= "<tr><td><b>Имя</b></td><td>$name</td></tr>";
+	$message .= "<tr><td><b>Контактный телефон</b></td><td>$phone</td></tr>";
+    $message .= "<tr><td><b>E-mail</b></td><td>$email</td></tr>";
+    $message .= "<tr><td><b>Город</b></td><td>$city</td></tr>";
+	$message .= "<tr><td colspan=\"2\">Сообщение создано автоматически!</td></tr>";
+	$message .= "</table>";
+				
+	mail($to, $subject, $message, $headers);
+
+	$jsonout = '{"status": "success", "message": "Заявка принята! Менеджер свяжется с Вами в ближайшее (рабочее) время."}';
+
+} else {
+
+	$jsonout = '{status": "error", "message": "Без телефона мы не сможем связаться с вами."}';
+	
+}
+
+echo $jsonout;
+
+?>
--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,68 @@
-var Util = {
-    randomInteger: function(min, max) {
-        var rand = min + Math.random() * (max - min)
-        rand = Math.round(rand);
-        return rand;
-    },
-    scrollToEl: function(el, offset) {
-        $("html,body").animate({ scrollTop: el.offset().top + (offset || 0) }, 500);
-    },
-    trimString: function(string) {
-        return string.split(' ').join('');
+var ajax = function(form) {
+  var formtarget = form,
+    data = $(formtarget).serialize();
+  // jqxhr = $.post("./ajax.php", data, onAjaxSuccess);
+
+  onAjaxSuccess(
+    '{"status": "success", "message": "Заявка принята! Менеджер свяжется с Вами в ближайшее (рабочее) время."}'
+  );
+
+  function onAjaxSuccess(data) {
+    var json = JSON.parse(data),
+      status = json.status,
+      message = json.message,
+      formid = json.formid;
+
+    if (status === "success") {
+      $(formtarget).addClass("disabled");
+
+      reachGoal("form");
     }
+
+    addNotify(status, message, $(formtarget).find(".form__result"));
+  }
+};
+
+var addNotify = function(status, msg, el) {
+  var responseEl = $(el)
+    .removeClass("success error")
+    .addClass(status)
+    .text(msg);
+
+  //   status === "success" && Shop._clearBasket();
+};
+
+var reachGoal = function(id, google) {
+  //   yaCounter55705045.reachGoal(id);
+  //   google && ga("send", "event", "forma", "zakaz");
+};
+
+var playerVideo = document.querySelector(".player__video"),
+  player = $(".player"),
+  playBtn = $(".player__play");
+
+function onYouTubeIframeAPIReady() {
+    playBtn.on("click", function() {
+        var player = new YT.Player(playerVideo, {
+            height: "100",
+            width: "100",
+            videoId: "GTRAEpllGZo",
+            events: {
+              onReady: onPlayerReady
+            }
+          });
+      });
+  
+}
+
+function onPlayerReady(event) {
+        event.target.playVideo();
+        player.addClass("playing");
 }
 
 $(function() {
-
+  $(".form").on("submit", function(e) {
+    e.preventDefault();
+    ajax(this);
+  });
 });
